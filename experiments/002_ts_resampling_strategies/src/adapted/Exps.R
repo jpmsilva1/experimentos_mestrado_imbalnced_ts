39bf22bbdd3de8e20a5f143cf137512b032dfbab
João Pedro Miranda <jpmsilva02@gmail.com>
2026-08-06 16:58:04 -0300

    fix(exp002): use parallelMap::parallelStartMulticore and cluster = TRUE for performanceEstimation

--- experiments/002_ts_resampling_strategies/src/adapted/Exps.R
+++ experiments/002_ts_resampling_strategies/src/adapted/Exps.R
@@ -4,6 +4,7 @@
 library(lubridate)
 library(xts)
 library(performanceEstimation)
+library(parallelMap)
 library(uba)
 library(UBL) # only for distance functions
 library(DMwR)
@@ -2538,6 +2539,9 @@ for (i in seq_along(data)) {
   minsplit <- 10
   cp <- 0.001
   
+  ncores <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", "48"))
+  parallelStartMulticore(cpus = ncores)
+  
   exp <- performanceEstimation(PredTask(form,ds),
                                c(Workflow("mc.lm"),
                                  Workflow("mc.lm_UNDERB"),
@@ -2592,8 +2596,10 @@ for (i in seq_along(data)) {
                                  Workflow("mc.arima"),
                                  Workflow("mc.BDES")),
                                EstimationTask("totTime",method=MonteCarlo(nReps=50,szTrain=.5,szTest=.25)),
-                               cluster = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", "48"))
+                               cluster = TRUE
   )
+  
+  parallelStop()
   
   save(exp, file = output_file)
   rm(exp, ds)
